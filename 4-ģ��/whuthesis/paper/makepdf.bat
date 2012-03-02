39d6b116d60aba1c1d225f4653908b284202a661
maria <happyfairy106@163.com>
2012-03-02 23:05:53 +0800

    移出部分文件。 新创建了一个简版的软件衰退综述列表； 粗略撰写了基于时间模型的研究现状；

--- 4-ģ��/whuthesis/paper/makepdf.bat
+++
@@ -1,6 +0,0 @@
-del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.ist *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi *.ps data\*.aux
-xelatex thesis
-bibtex thesis
-bibtex thesis
-xelatex thesis
-xelatex thesis
